81a8c587db54d69220326972bf812e5a942f868b
kjwen310 <kjwen310@gmail.com>
2024-05-26 23:53:20 +0800

    feat: add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-y-4 bg-slate-200">
+      <div className="relative max-w-lg h-[400px] aspect-video">
+        <Image
+          src="/not-found.svg"
+          fill
+          alt="not-found"
+          className="bg-cover bg-center"
+        />
+      </div>
+      <Button size="lg" variant="ghost" asChild>
+        <Link href="/">Back To Landing Page</Link>
+      </Button>
+    </div>
+  );
+}
