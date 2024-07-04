7ef4d12db54c93971420f230f6452c616a96b5e3
kjwen310 <kjwen310@gmail.com>
2024-07-04 12:14:35 +0800

    chore: add `suppressHydrationWarning` to avoid warning caused by next-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn('bg-white dark:bg-[#313338]', inter.className)}>
         <QueryProvider>
           <ThemeProvider
